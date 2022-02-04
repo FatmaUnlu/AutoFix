1f61f1d6db948755aaa292b4c1d86616baeb32f4
fatmaunlu <fatmaunluu93@gmail.com>
2022-02-04 11:51:21 +0300

    AdminAreaView

---
+++ AutoFix/Areas/Admin/Controllers/UserApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AutoFix.Areas.Admin.Controllers
+{
+    public class UserApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
