6a3ba8cb73bfddb987c569791c337d81f0c6b6b6
fatmaunlu <fatmaunluu93@gmail.com>
2022-02-04 14:51:08 +0300

    admin arayüz giydirildi

---
+++ AutoFix/Areas/Admin/Controllers/ManageController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AutoFix.Areas.Admin.Controllers
+{
+    public class ManageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult User()
+        {
+            return View();
+        }
+    }
+}
