2b90ed2ff9bf4cbb388cb8d974c637e9419952cd
Fulya Sarıyar Abdullah <fulya12agustos@hotmail.com>
2022-02-05 17:36:59 +0300

    Register v1

--- AutoFix/Controllers/AccountController.cs
+++ AutoFix/Controllers/AccountController.cs
@@ -1,14 +1,77 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoFix.Models.Identity;
+using AutoFix.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace AutoFix.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Login()
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Register()
         {
             return View();
         }
-        public IActionResult Register()
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.Password = string.Empty;
+                model.ConfirmPassword = string.Empty;
+                return View(model);
+            }
+
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user != null)
+            {
+                ModelState.AddModelError(nameof(model.UserName), "Bu kullanıcı adı daha önce sisteme kayıt edilmiştir");
+                return View(model);
+            }
+            user = await _userManager.FindByEmailAsync(model.Email);
+            if (user != null)
+            {
+                ModelState.AddModelError(nameof(model.Email), "Bu email daha önce sisteme kayıt edilmiştir");
+                return View(model);
+            }
+
+            user = new ApplicationUser()
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                Name = model.Name,
+                Surname = model.Surname
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                //kullanıcıya rol atama
+                //kullanıcıya email doğrulama gönderme
+                //giriş sayfasına yönlendirme
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Kayıt işleminde bir hata oluştu");
+                return View(model);
+            }
+
+
+            return View();
+        }
+
+
+        public IActionResult Login()
         {
             return View();
         }
