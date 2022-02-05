b1fb6efa3674579bb934ee5e621604ea0a0dd671
fatmaunlu <fatmaunluu93@gmail.com>
2022-02-05 22:58:13 +0300

    area login

--- AutoFix/Areas/Admin/Controllers/ManageController.cs
+++ AutoFix/Areas/Admin/Controllers/ManageController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AutoFix.Areas.Admin.Controllers
 {
@@ -9,6 +10,12 @@ namespace AutoFix.Areas.Admin.Controllers
             return View();
         }
         public IActionResult User()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login()
         {
             return View();
         }
