c413c38c18887a91395cff9ac9a9037043901eb8
fatmaunlu <fatmaunluu93@gmail.com>
2022-02-07 20:06:03 +0300

    login-logout işlemleri

--- AutoFix/ViewModels/LoginViewModel.cs
+++ AutoFix/ViewModels/LoginViewModel.cs
@@ -4,8 +4,10 @@ namespace AutoFix.ViewModels
 {
     public class LoginViewModel
     {
+        [Display(Name = "Kullanıcı Adı")]
         [Required(ErrorMessage = "Kullanıcı adı alanı gereklidir.")]
         public string UserName { get; set; }
+
         [Required(ErrorMessage = "Şifre alanı gereklidir.")]
         //[StringLength(100),MinLength(6,ErrorMessage ="Şifreniz minimum 6 karekterli olmalıdır.")]
         [StringLength(100, MinimumLength = 6, ErrorMessage = "Şifreniz minimum 6 karekterli olmalıdır.")]
@@ -13,6 +15,8 @@ namespace AutoFix.ViewModels
         [Display(Name = "Şifre")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
-      
+        [Display(Name = "Beni Hatırla")]
+        public bool RememberMe { get; set; }
+
     }
 }
