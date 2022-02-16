9055d20efe138c881494fda6be59f6522380ccac
fatmaunlu <fatmaunluu93@gmail.com>
2022-02-16 10:42:39 +0300

    RoleAssign CheckBox

--- AutoFix/Areas/Admin/Controllers/ManageController.cs
+++ AutoFix/Areas/Admin/Controllers/ManageController.cs
@@ -1,10 +1,24 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AutoFix.Models.Identity;
+using AutoFix.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AutoFix.Areas.Admin.Controllers
 {
     public class ManageController : AdminBaseController
     {
+        private readonly RoleManager<AplicationRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ManageController(RoleManager<AplicationRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
         public IActionResult Index()
         {
             return View();
@@ -19,11 +33,6 @@ namespace AutoFix.Areas.Admin.Controllers
         {
             return View();
         }
-
-        public IActionResult Role()
-        {
-            return View();
-        }
         public IActionResult ServiceProducts()
         {
             return View();
@@ -31,6 +40,36 @@ namespace AutoFix.Areas.Admin.Controllers
         public IActionResult Deneme()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> RoleAssign(string id)
+        {
+            ApplicationUser user = await _userManager.FindByIdAsync(id);
+            List<AplicationRole> allRoles = _roleManager.Roles.ToList();
+            List<string> userRoles = await _userManager.GetRolesAsync(user) as List<string>;
+            List<RoleAssignViewModel> assignRoles = new List<RoleAssignViewModel>();
+            allRoles.ForEach(role => assignRoles.Add(new RoleAssignViewModel
+            {
+                HasAssign = userRoles.Contains(role.Name),
+                RoleId = role.Id,
+                RoleName = role.Name
+            }));
+
+            return View(assignRoles);
+        }
+        [HttpPost]
+        public async Task<ActionResult> RoleAssign(List<RoleAssignViewModel> modelList, string id)
+        {
+            ApplicationUser user = await _userManager.FindByIdAsync(id);
+            foreach (RoleAssignViewModel role in modelList)
+            {
+                if (role.HasAssign)
+                    await _userManager.AddToRoleAsync(user, role.RoleName);
+                else
+                    await _userManager.RemoveFromRoleAsync(user, role.RoleName);
+            }
+            return RedirectToAction("Users", "Manage");
         }
     }
 }
---
+++ AutoFix/Areas/Admin/ViewModels/RoleAssignViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AutoFix.ViewModels
+{
+    public class RoleAssignViewModel
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+        public bool HasAssign { get; set; }
+    }
+}
